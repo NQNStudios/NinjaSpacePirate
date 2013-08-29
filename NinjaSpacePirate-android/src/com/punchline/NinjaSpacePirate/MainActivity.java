e0fd782c5c38e8363555cc8c6e49e1705c0d41d1
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-08-29 14:15:15 -0600

    Added the Android project.

---
+++ NinjaSpacePirate-android/src/com/punchline/NinjaSpacePirate/MainActivity.java
@@ -0,0 +1,18 @@
+package com.punchline.NinjaSpacePirate;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new NinjaSpacePirate(), cfg);
+    }
+}
